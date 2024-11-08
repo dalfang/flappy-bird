d171bb76aaf778dc18a70dc4b263212936987b59
[Dalin Fangloy] <[d.fangloy@gmail.com]>
2024-11-08 15:10:59 +0100

    set up animations

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import { Canvas, useImage, Image } from "@shopify/react-native-skia";
+import { useEffect } from "react";
 import { useWindowDimensions } from "react-native";
+import {
+  useSharedValue,
+  withTiming,
+  Easing,
+  withSequence,
+  withRepeat,
+} from "react-native-reanimated";
 
 const App = () => {
   const { width, height } = useWindowDimensions();
@@ -9,6 +17,17 @@ const App = () => {
   const pipeBottom = useImage(require("./assets/sprites/pipe-green.png"));
   const pipeTop = useImage(require("./assets/sprites/pipe-green-top.png"));
   const base = useImage(require("./assets/sprites/base.png"));
+
+  const x = useSharedValue(width);
+  useEffect(() => {
+    x.value = withRepeat(
+      withSequence(
+        withTiming(-150, { duration: 3000, easing: Easing.linear }),
+        withTiming(width, { duration: 0 })
+      ) - 1
+    );
+  }, []);
+
   const pipeOffset = 0;
 
   return (
@@ -16,25 +35,25 @@ const App = () => {
       {/*back ground*/}
       <Image image={bg} width={width} height={height} fit={"cover"} />
 
-      {/*base*/}
-      <Image image={base} y={height - 75} x={0} width={width} height={150} />
-
       {/*pipe*/}
       <Image
         image={pipeTop}
         y={pipeOffset - 320}
-        x={width / 2}
-        width={103}
-        height={640}
-      />
-      <Image
-        image={pipeBottom}
-        y={height - 300 + pipeOffset}
-        x={width / 2}
-        width={103}
+        x={x}
+        width={104}
         height={640}
       />
 
+      <Image
+        image={pipeBottom}
+        y={height - 300 + pipeOffset}
+        x={x}
+        width={104}
+        height={640}
+      />
+
+      {/*base*/}
+      <Image image={base} y={height - 75} x={0} width={width} height={150} />
       {/*bird*/}
       <Image image={bird} y={height / 2} x={width / 4} width={64} height={48} />
       {/*if we want the bird to be at the center we should do height /2 - "the
